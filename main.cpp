780fecd1a7d995a2ce9a3986bb701703ca68cc9b
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-06-17 06:43:00 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <QApplication>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QRandomGenerator>
+
+class DiceRoller : public QWidget {
+    Q_OBJECT
+
+public:
+    DiceRoller(QWidget *parent = nullptr);
+
+private slots:
+    void rollDice();
+
+private:
+    QLabel *resultLabel;
+    QPushButton *rollButton;
+};
+
+DiceRoller::DiceRoller(QWidget *parent) : QWidget(parent) {
+    resultLabel = new QLabel("Roll the dice!", this);
+    resultLabel->setAlignment(Qt::AlignCenter);
+
+    rollButton = new QPushButton("Roll", this);
+    connect(rollButton, &QPushButton::clicked, this, &DiceRoller::rollDice);
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(resultLabel);
+    layout->addWidget(rollButton);
+    setLayout(layout);
+
+    setWindowTitle("Dice Roller");
+    resize(200, 100);
+}
+
+void DiceRoller::rollDice() {
+    int result = QRandomGenerator::global()->bounded(1, 7); // Generates a random number between 1 and 6
+    resultLabel->setText("You rolled: " + QString::number(result));
+}
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    DiceRoller diceRoller;
+    diceRoller.show();
+    return app.exec();
+}
+
+#include "main.moc"
